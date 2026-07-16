8f85e2a42606cf5bb63cd1cde35c376a70827899
AhmetTuran <akio@msn.com>
2026-07-16 15:16:11 +0300

    null error fix for pdf

---
+++ WeeklyCustomReportGenerator/SafePdfCanvasProcessor.cs
@@ -0,0 +1,25 @@
+﻿namespace WeeklyCustomReportGenerator;
+
+using iText.IO.Font.Constants;
+using iText.Kernel.Font;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+
+public class SafePdfCanvasProcessor(ITextExtractionStrategy strategy) : PdfCanvasProcessor(strategy)
+{
+    protected override PdfFont GetFont(PdfDictionary fontDict)
+    {
+        if (fontDict == null)
+            return PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+
+        try
+        {
+            return base.GetFont(fontDict);
+        }
+        catch
+        {
+            return PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+        }
+    }
+}
